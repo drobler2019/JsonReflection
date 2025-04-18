404e12719f302b62a444c122c4657433842ed013
drobler2019 <drobler2019@gmail.com>
2025-04-18 11:41:12 -0500

    Prueba unitaria al método que convierte json en mapas

---
+++ src/test/java/com/app/service/ValidateFormatJsonImplTest.java
@@ -0,0 +1,49 @@
+package com.app.service;
+
+
+import com.app.interfaces.ValidateFormatJsonService;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class ValidateFormatJsonImplTest {
+
+    private final ValidateFormatJsonService validateFormatJsonService = new ValidateFormatJsonImpl();
+    private final Map<String, String> map = new HashMap<>();
+
+    @BeforeEach
+    public void init() {
+        map.put("nombre", "Maria");
+        map.put("apellido", "Rubio");
+        map.put("edad", "15");
+    }
+
+    @Test
+    public void getMapTest() {
+        String key = "nombre";
+        String values = "[matemáticas,sociales]";
+        String object = "{\"value\": false}";
+        String json = "{\"nombre\" : \"Diego\", \"subjects\": [\"matemáticas\",\"sociales\"],\"status\": {\"value\": false}}";
+        String value = map.get(key);
+        var mapResult = validateFormatJsonService.getMap("{\"nombre\" : \"Maria\"}");
+        var otherResult = validateFormatJsonService.getMap("{\"nombre\" : \"Diego\"}");
+        String subjects = validateFormatJsonService.getMap(json).get("subjects");
+        String status = validateFormatJsonService.getMap(json).get("status");
+
+        assertAll(() -> assertEquals(value, mapResult.get(key)),
+                () -> assertNotEquals(value, otherResult.get(key)),
+                () -> assertEquals(values, subjects),
+                () -> assertEquals(object,status));
+    }
+
+    @Test
+    public void getMapExceptionTest() {
+        String json = "}";
+        assertThrows(IllegalArgumentException.class, () -> validateFormatJsonService.getMap(json));
+    }
+}
